7f1bd57217a96b3a56d8fc9605b7353b192eb623
malyshewandrew <malyshewandrew@protonmail.com>
2023-12-24 15:05:32 +0300

    First commit

---
+++ CriptoExchange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CriptoExchange
+//
+//  Created by MALYSHEW ANDREW on 24.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
